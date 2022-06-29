48b1fee8fbd98b029fce4b938e0cd05309744e8d
mauro137 <maurolizarriaga2@gmail.com>
2022-06-29 19:34:46 -0300

    asd

---
+++ javascript/form-contact.js
@@ -0,0 +1,7 @@
+const btnEnviar = document.getElementsByClassName("form__btn__enviar");
+const form = document.getElementById("contacto__form");
+const inputNombre = document.getElementById("form__nombre").value
+const inputCorreo = document.getElementById("form__email").value
+const inputAsunto = document.getElementById("form__asunto").value
+var inputMensaje = document.getElementById("form__mensaje").value
+
