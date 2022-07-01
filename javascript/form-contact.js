21d41432803606bceaec0ca12814368f86fad720
mauro137 <maurolizarriaga2@gmail.com>
2022-07-01 20:21:37 -0300

    control formulario listo, falta validacion y responsive

--- javascript/form-contact.js
+++ javascript/form-contact.js
@@ -1,7 +1,31 @@
-const btnEnviar = document.getElementsByClassName("form__btn__enviar");
-const form = document.getElementById("contacto__form");
-const inputNombre = document.getElementById("form__nombre").value
-const inputCorreo = document.getElementById("form__email").value
-const inputAsunto = document.getElementById("form__asunto").value
-var inputMensaje = document.getElementById("form__mensaje").value
+const btnEnviar = document.querySelector(".form__btn__enviar");
+const nombre = document.getElementById("form__nombre");
+const correo = document.getElementById("form__email");
+const asunto = document.getElementById("form__asunto");
+const mensaje = document.getElementById("form__mensaje");
+const formulario = document.getElementById("contacto__form");
+btnEnviar.disabled = true;
 
+formulario.addEventListener("keyup", formControl);
+btnEnviar.addEventListener("click", formValidation);
+
+function formControl() {
+  if (
+    nombre.value.length > 0 &&
+    correo.value.length > 0 &&
+    asunto.value.length > 0 &&
+    mensaje.value.length > 0
+  ) {
+    btnEnviar.disabled = false;
+  } else {
+    btnEnviar.disabled = true;
+  }
+}
+
+function formValidation() {
+  if (condition) {
+    if (condition) {
+    } else {
+    }
+  }
+}
