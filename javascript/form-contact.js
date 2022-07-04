9cc99eeb67c9e24868653c822bf9ea7c8619d359
mauro137 <maurolizarriaga2@gmail.com>
2022-07-04 00:07:27 -0300

    formulario completado, se agrego reset al formulario y mensaje de envio exitoso

--- javascript/form-contact.js
+++ javascript/form-contact.js
@@ -1,5 +1,5 @@
 const formulario = document.getElementById("contacto__form");
-const inputs = document.querySelectorAll("#contacto__form input");
+const inputs = document.querySelectorAll("#contacto__form input, textarea");
 const textarea = document.querySelector("#mensaje__box");
 const btnEnviar = document.querySelector(".form__btn__enviar");
 btnEnviar.disabled = true;
@@ -10,52 +10,42 @@ const asunto = document.getElementById("form__asunto");
 const mensaje = document.getElementById("form__mensaje");
 
 const expresiones = {
-  nombre_asunto: /^([A-Za-z]){1,50}$/,
+  nombre: /^([A-Za-z]){1,50}$/,
+  asunto: /^([A-Za-z0-9$!#?%=-]){1,50}$/,
   email:
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-  mensaje: /^([A-Za-z]){1,300}$/,
+  mensaje: /^([A-Za-z0-9!#%$+-?=*&".,]){1,300}$/,
 };
 
 const validarFormulario = (e) => {
   switch (e.target.name) {
     case "form__nombre":
-      validarCampo(expresiones.nombre_asunto, e.target, "nombre");
+      validarCampo(expresiones.nombre, e.target, "nombre");
       break;
     case "form__email":
       validarCampo(expresiones.email, e.target, "email");
       break;
     case "form__asunto":
-      validarCampo(expresiones.nombre_asunto, e.target, "asunto");
+      validarCampo(expresiones.asunto, e.target, "asunto");
       break;
     case "form__mensaje":
-      validarArea(expresiones.mensaje, e.target, "mensaje");
+      validarCampo(expresiones.mensaje, e.target, "mensaje");
       break;
   }
 };
-
-/* EVENTOS DE LOS ELEMENTOS DEL FORMULARIO PARA LA VALIDACION*/
-
-const validarArea = (expresion,  textarea, campo) => {
-  if (expresion.test(textarea.value)) {
-    document
-      .getElementById(`${campo}__box`)
-      .classList.remove("formulario__incorrecto");
-  } else {
-    document
-      .getElementById(`${campo}__box`)
-      .classList.add("formulario__incorrecto");
-  }
-};
+/* EVENTOS DE LOS ELEMENTOS DEL FORMULARIO PARA LA VALIDACION, SE USA TEMPLATE STRING PARA AHORRAR CODIGO Y DINAMIZAR*/
 
 const validarCampo = (expresion, input, campo) => {
+  let campoBox = document.getElementById(`${campo}__box`);
+  let grupoBox = document.querySelector(`#${campo}__grupo .input__error`);
   if (expresion.test(input.value)) {
-    document
-      .getElementById(`${campo}__box`)
-      .classList.remove("formulario__incorrecto");
+    campoBox.classList.remove("formulario__incorrecto");
+    grupoBox.classList.remove("input__error-mostrar");
+    campoBox.classList.add("formulario__correcto");
   } else {
-    document
-      .getElementById(`${campo}__box`)
-      .classList.add("formulario__incorrecto");
+    campoBox.classList.remove("formulario__correcto");
+    grupoBox.classList.add("input__error-mostrar");
+    campoBox.classList.add("formulario__incorrecto");
   }
 };
 textarea.addEventListener("keyup", validarFormulario);
@@ -66,13 +56,10 @@ inputs.forEach((input) => {
   input.addEventListener("blur", validarFormulario);
 });
 
-/* Control del boton enviar */
-formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+
+/* controla que los campos esten completos y activa/desactiva el btn de envio de forma dinamica */
 
 formulario.addEventListener("keyup", formControl);
-
 function formControl() {
   if (
     nombre.value.length > 0 &&
@@ -85,3 +72,18 @@ function formControl() {
     btnEnviar.disabled = true;
   }
 }
+
+/* reset del formulario y los estilos*/
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formulario.reset();
+
+document.querySelector(".form__enviado").classList.add("form__enviado-visible")
+  setTimeout(() => {
+    document.querySelector(".form__enviado").classList.remove("form__enviado-visible")
+  }, 5000);
+
+  document.querySelectorAll(".formulario__correcto").forEach((campo_valido) => {
+    campo_valido.classList.remove("formulario__correcto");
+  });
+});
